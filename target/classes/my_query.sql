b03277f52e8a93465f52af184eaf05bf2980fdb2
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2021-01-22 08:06:45 -0400

    Remoção da pasta target

--- target/classes/my_query.sql
+++
@@ -1,8 +0,0 @@
-create table todo(
-         id bigint primary key auto_increment,
-         description varchar(250),
-         done Bool default false,
-         register_date datetime,
-         done_date datetime
-);
-
